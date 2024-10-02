2dfcf86bb976adb7cbc1fd3fd31ea14eea6a077e
leolin2351 <leol62@nycstudents.net>
2024-10-02 14:48:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Leo"; // update the part BETWEEN the "quotes"
+let userName = "leolin2351.github.io"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
